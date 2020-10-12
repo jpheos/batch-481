c9c9c606acb676cb90c5c3665f3dda776b8ebf1d
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-10-12 12:19:56 +0200

    Reboot-02-Horse-race correction first part

--- 01-Ruby/05-Regular-Expressions/Reboot/Reboot-02-Horse-race/lib/interface.rb
+++ 01-Ruby/05-Regular-Expressions/Reboot/Reboot-02-Horse-race/lib/interface.rb
@@ -2,15 +2,41 @@
 
 # Pseudo-code:
 
-definir un array(String) de chevaux
+# definir un array(String) de chevaux
 
-1. Print welcome and the horses names
-  - puts
-2. Get user's bet
-  - on lui affiche les choix possibles
-    - puts / index / + 1
-  - choice(index) gets.chomp / - 1
-3. Run the race 🐴
-  - random + nb horses
-4. Print
-  - puts
+
+# 1. Print welcome and the horses names
+#   - puts
+# 2. Get user's bet
+#   - on lui affiche les choix possibles
+#     - puts / index / + 1
+#   - choice(index) gets.chomp / - 1
+# 3. Run the race 🐴
+#   - random + nb horses
+# 4. Print
+#   - puts
+
+horses = %W[toto titi tata]
+
+puts "Welcome!!!"
+
+horses.each_with_index do |horse, i|
+  puts "#{i + 1} - #{horse}"
+end
+
+puts "your choice?"
+print "> "
+
+choice = gets.chomp.to_i - 1
+
+winner = rand(horses.size)
+
+
+if choice == winner
+  puts "WIN"
+else
+  puts "LOOSER"
+end
+
+
+
