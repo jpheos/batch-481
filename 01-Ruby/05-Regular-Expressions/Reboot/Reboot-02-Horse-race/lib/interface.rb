21bfb23b2551461dbf73650e02772b283e9f4077
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-10-12 11:43:25 +0200

    Reboot-02-Horse-race add speudo code

---
+++ 01-Ruby/05-Regular-Expressions/Reboot/Reboot-02-Horse-race/lib/interface.rb
@@ -0,0 +1,16 @@
+# interface.rb
+
+# Pseudo-code:
+
+definir un array(String) de chevaux
+
+1. Print welcome and the horses names
+  - puts
+2. Get user's bet
+  - on lui affiche les choix possibles
+    - puts / index / + 1
+  - choice(index) gets.chomp / - 1
+3. Run the race 🐴
+  - random + nb horses
+4. Print
+  - puts
