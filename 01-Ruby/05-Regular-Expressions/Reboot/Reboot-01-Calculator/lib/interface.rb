f430e53a4ae7aa5f2df573ef410d18afdef1a569
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-10-12 10:43:49 +0200

    Reboot-01-Calculator speudo code

---
+++ 01-Ruby/05-Regular-Expressions/Reboot/Reboot-01-Calculator/lib/interface.rb
@@ -0,0 +1,16 @@
+# TODO: you can build your calculator program here!
+
+
+
+on demande un nombre à l'utilisateur
+  - puts + gets.chomp (nb1)
+on demande un deuxieme nombre
+  - puts + gets.chomp (nb2)
+on lui demande un operator
+  - on lui affiche une liste d'operateurs possible
+    - puts
+  - on enregistre son choix
+    - gets.chomp
+on calcul un résultat
+  - method do_calcul(nb1:Integer, nb2:Integer, oeprator:String)
+on affiche un résultat
