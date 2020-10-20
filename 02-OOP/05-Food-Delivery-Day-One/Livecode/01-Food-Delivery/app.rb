bdd93d68fda1963b0e25ed27b7c8f79012fcc379
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2020-10-20 18:26:45 +0200

    add /02-OOP/05-Food-Delivery-Day-One/Livecode

---
+++ 02-OOP/05-Food-Delivery-Day-One/Livecode/01-Food-Delivery/app.rb
@@ -0,0 +1,18 @@
+# TODO: require relevant files to bootstrap the app.
+# Then you can test your program with:
+#   ruby app.rb
+
+require_relative 'router'
+require_relative 'app/repositories/customer_repository'
+require_relative 'app/repositories/meal_repository'
+require_relative 'app/controllers/customers_controller'
+require_relative 'app/controllers/meals_controller'
+
+meal_repository = MealRepository.new('data/meals.csv')
+meals_controller = MealsController.new(meal_repository)
+
+customer_repository = CustomerRepository.new('data/customers.csv')
+customers_controller = CustomersController.new(customer_repository)
+
+router = Router.new(meals_controller, customers_controller)
+router.run
